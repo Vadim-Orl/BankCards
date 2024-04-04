942d42cbaefab20a1e023351bcc9564217ed831c
vad-orl <vad-orl90@mail.ru>
2024-04-05 01:50:25 +0300

    изменен на http

--- BankCards/src/shared/api/baseApi.ts
+++ BankCards/src/shared/api/baseApi.ts
@@ -4,7 +4,7 @@ import { RootState } from '../../app/AppStore';
 export const baseApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://devapp.bonusmoney.pro/mobileapp/getAllCompanies',
+        baseUrl: 'http://devapp.bonusmoney.pro/mobileapp/getAllCompanies',
         prepareHeaders: (headers, { getState }) => {
             const token = (getState() as RootState).data.token;
             if (token) {
